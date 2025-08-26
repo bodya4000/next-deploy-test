fce8399ac632e778cfc7ab9e3a77206e71680b90
Bohdan Shraier <bohdanshraier@Bohdans-MacBook-Air.local>
2025-08-26 09:18:05 +0300

    fix: deep link

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -5,20 +5,20 @@ export default function OpenAppPage() {
   useEffect(() => {
     const userAgent = navigator.userAgent || navigator.vendor
 
-    const deepLink = 'myapp://open' 
+    const deepLink = 'myapp://list' 
     window.location.href = deepLink
 
-    const timer = setTimeout(() => {
-      if (/android/i.test(userAgent)) {
-        window.location.href = 'https://play.google.com/store/apps/details?id=com.myapp'
-      } else if (/iPad|iPhone|iPod/.test(userAgent)) {
-        window.location.href = 'https://apps.apple.com/app/id1234567890'
-      } else {
-        window.location.href = '/'
-      }
-    }, 1500)
+    // const timer = setTimeout(() => {
+    //   if (/android/i.test(userAgent)) {
+    //     window.location.href = 'https://play.google.com/store/apps/details?id=com.myapp'
+    //   } else if (/iPad|iPhone|iPod/.test(userAgent)) {
+    //     window.location.href = 'https://apps.apple.com/app/id1234567890'
+    //   } else {
+    //     window.location.href = '/'
+    //   }
+    // }, 1500)
 
-    return () => clearTimeout(timer)
+    // return () => clearTimeout(timer)
   }, [])
 
   return <p>Opening app...</p>
